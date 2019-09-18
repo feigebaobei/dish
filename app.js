3aae60dd25a91919f93a12b9a00b8fd85cebc0ac
feigebaobei <18515195415@163.com>
2019-09-18 20:08:31 +0800

    use https

--- app.js
+++ app.js
@@ -10,12 +10,20 @@ var users = require('./routes/users');
 
 var app = express();
 
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next()
+  } else {
+    res.redirect(307, `https://${req.hostname}:${app.get('secPort')}${req.url}`)
+  }
+})
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
